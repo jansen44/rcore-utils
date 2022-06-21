c6f08e6d2cfd519f53ccc6caf48e86bee4faeced
Jansen Junior <jsd.alcantara@gmail.com>
2022-06-20 20:45:13 -0400

    feat: yes

--- src/bin/yes/main.rs
+++ src/bin/yes/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("TODO: implement yes");
+    let mut argv = env::args();
+    let operand_lim = match argv.nth(1) {
+        Some(arg) => arg,
+        None => String::from("y"),
+    };
+
+    // TODO: Optimize output like gnu/core-utils "yes"
+
+    loop {
+        println!("{operand_lim}");
+    }
 }
